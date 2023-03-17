da3e4a54678ab1ea89b936a0cf1dbcd319b487ed
LuHernane <101757175+LuHernane@users.noreply.github.com>
2023-03-17 15:58:01 -0300

    arrumou as pastas

--- src/loguin/index.tsx
+++ src/loguin/index.tsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-
 export default function Home() {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   const userId = "redmi";
-  const passwordId = "note";
+  const passwordId = "note8";
 
   function Loguin() {
-    if (userId === "redmi" && passwordId === "note") {
+    if (userId === "redmi" && passwordId === "note8") {
     }
   }
 
   return null;
+}
+function useState(arg0: string): [any, any] {
+  throw new Error("Function not implemented.");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>,
+
+  document.getElementById("root")
+);
