70ee6075b5ad3980bdd9665b4aaa3f219b07d177
LuHernane <101757175+LuHernane@users.noreply.github.com>
2023-03-17 14:55:07 -0300

    teste

---
+++ src/loguin/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const userId = "redmi";
+  const passwordId = "note";
+
+  function Loguin() {
+    if (userId === "redmi" && passwordId === "note") {
+    }
+  }
+
+  return null;
+}
---
+++ src/routs/root.tsx
@@ -0,0 +1,18 @@
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+import chakraTheme from "@chakra-ui/theme";
+
+const { Button } = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
+
+function App() {
+  return (
+    <ChakraBaseProvider theme={theme}>
+      <App />
+    </ChakraBaseProvider>
+  );
+}
