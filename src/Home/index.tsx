a43ccd7f033a5164f857acfc2601e65729a31af6
LuHernane <101757175+LuHernane@users.noreply.github.com>
2023-03-20 13:51:43 -0300

    stack na home

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,14 +1,17 @@
 import { Box } from "@chakra-ui/layout";
+import { Stack } from "@chakra-ui/react";
 import NavBar from "../../componentes/NavBar";
 
 export default function Home() {
   return (
     <Box bg="gray" h="container.xl" w="100%">
       <NavBar />
-      <h1> my home</h1>
-      <h1> my home</h1>
-      <h1> my home</h1>
-      <h1> my home</h1>
+      <Stack align={"center"} mt="25px">
+        <h1> my home</h1>
+        <h1> my home</h1>
+        <h1> my home</h1>
+        <h1> my home</h1>
+      </Stack>
     </Box>
   );
 }
